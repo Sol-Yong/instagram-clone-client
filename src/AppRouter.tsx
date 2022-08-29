f64c80f0ef4c81582bc1f3b962d911f69d5fa7cb
Yongveloper <aaa9015@naver.com>
2022-08-29 18:21:45 +0900

    [구성] 불필요한 파일 삭제

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainLogin from './Pages/MainLogin';
 import AccountJoin from './Pages/Account/AccountJoin';
