95bfc211f0c0a41ab553c2f491bb25ae268e1bbc
yongveloper <aaa9015@naver.com>
2022-08-30 15:58:22 +0900

    [스타일] SerachBar focus 스타일링

--- src/Components/Common/SearchBar.tsx
+++ src/Components/Common/SearchBar.tsx
@@ -1,4 +1,6 @@
+import { useRef, useState } from 'react';
 import styled from 'styled-components';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 const Container = styled.div`
   border-radius: 8px;
@@ -21,21 +23,96 @@ const SearchInput = styled.input`
   max-width: 268px;
   min-width: 125px;
   border: 0;
-  margin-left: 24px;
   background: rgb(239, 239, 239);
   border-radius: 8px;
-  box-sizing: border-box;
   color: rgb(38, 38, 38);
   font-size: 16px;
   height: 100%;
   outline: none;
+  z-index: 2;
   padding: 3px 16px;
+  line-height: 18px;
+`;
+
+const PlaceholderContanier = styled.div<{ focused: boolean }>`
+  background: rgb(239, 239, 239);
+  display: ${(props) => (props.focused ? 'none' : 'flex')};
+  flex-direction: column;
+  font-size: 16px;
+  padding: 0 16px;
+  color: rgb(142, 142, 142);
+  cursor: text;
+  font-weight: 300;
+  justify-content: center;
+  left: 0;
+  position: absolute;
+  text-align: left;
+  top: 0;
+  z-index: 2;
+  border-radius: 8px;
+  height: 100%;
+  width: 100%;
+
+  &:focus {
+    display: none;
+  }
+
+  & > div > div {
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+  }
+
+  & svg {
+    margin-right: 8px;
+  }
+
+  & span {
+    line-height: 25px;
+  }
 `;
 
 function SearchBar() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [value, setValue] = useState('');
+  const [focused, setFocused] = useState(false);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const handleFocus = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+    setFocused(true);
+  };
+
+  const handleBlur = () => {
+    setFocused(false);
+  };
+
   return (
-    <Container>
-      <SearchInput placeholder="검색" type="text" />
+    <Container onClick={handleFocus}>
+      <SearchInput
+        ref={inputRef}
+        placeholder="검색"
+        type="text"
+        value={value}
+        onChange={handleInputChange}
+        onBlur={handleBlur}
+      />
+      <PlaceholderContanier focused={focused}>
+        <div>
+          <div>
+            <SearchOutlinedIcon />
+            <span>{value.length > 0 ? value : '검색'}</span>
+          </div>
+        </div>
+      </PlaceholderContanier>
     </Container>
   );
 }
